91c4b487d967de16399c91f4d57f4d8d9c212e12
Gabriel Marca <brielmarca@gmail.com>
2026-07-13 22:48:17 -0300

    feat: enable translated interactive portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,5 @@
-import { AboutSection } from "@/components/AboutSection";
-import { ContactSection } from "@/components/ContactSection";
-import { Footer } from "@/components/Footer";
-import { Header } from "@/components/Header";
-import { HeroSection } from "@/components/HeroSection";
-import { ProjectsSection } from "@/components/ProjectsSection";
-import { SkillsSection } from "@/components/SkillsSection";
+import { PortfolioApp } from "@/components/PortfolioApp";
 
 export default function Home() {
-  return (
-    <>
-      <a className="skip-link" href="#main-content">Skip to main content</a>
-      <div className="ambient ambient-purple" aria-hidden="true" />
-      <div className="ambient ambient-blue" aria-hidden="true" />
-      <Header />
-      <main id="main-content" tabIndex={-1}>
-        <HeroSection />
-        <AboutSection />
-        <SkillsSection />
-        <ProjectsSection />
-        <ContactSection />
-      </main>
-      <Footer />
-    </>
-  );
+  return <PortfolioApp />;
 }
